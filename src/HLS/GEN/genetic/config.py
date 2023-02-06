080d0ad5c915cd4ce18f9fcb4aed158fb6cb50d6
wurifu desktop <rifuwu123@gmail.com>
2023-02-07 00:46:19 +0800

    230207 统一参数，准备测试

--- src/HLS/GEN/genetic/config.py
+++ src/HLS/GEN/genetic/config.py
@@ -1,13 +1,13 @@
 import os
 
 POP_SIZE = 50
-CHROM_LENGTH = 35
-GEN_NUM = 150
+CHROM_LENGTH = 70
+GEN_NUM = 100
 
 PROBLEM_SET = ['MK01', 'MK02', 'MK03', 'MK04', 'MK05', 'MK06', 'MK07', 'MK08', 'MK09', 'MK10']
-PROBLEM = 'MK05'
+PROBLEM = 'MK04'
 PROBLEM_PATH = os.path.join(os.getcwd(), "../../../Brandimarte_Data/" + PROBLEM + ".fjs")
 
 P_MUT = 0.3
 
-CROSS_TIMES = 10
+CROSS_TIMES = 5
--- src/HLS/GEN/genetic/gen_main.py
+++ src/HLS/GEN/genetic/gen_main.py
@@ -34,12 +34,12 @@ def run(problem):
                 best_time = new_time
                 best_solution = new_solutions[i]
                 print('new best time: ', best_time)
-            elif new_time == best_time:
-                p = random.random()
-                if p < 0.6 - 0.005 * gen:
-                    best_time = new_time
-                    best_solution = new_solutions[i]
-                    print('new best time: ', best_time)
+            # elif new_time == best_time:
+            #     p = random.random()
+            #     if p < 0.6 - 0.005 * gen:
+            #         best_time = new_time
+            #         best_solution = new_solutions[i]
+            #         print('new best time: ', best_time)
 
         #子代选择两个个体
         childChrom1 = gen_ops.bestSelection(childPop, fitness)
@@ -53,32 +53,21 @@ def runForTest(problem):
     parameters = parse(problem)
     best_solution = initializeResult(parameters)
     best_time = timeTaken(best_solution, parameters)
-
-    #初始化种群
     population = gen_ops.initPopulation(config.POP_SIZE, config.CHROM_LENGTH)
-    #开始循环
-    for i in range(config.GEN_NUM):
-        # 随机选出两个父代
+    for gen in range(config.GEN_NUM):
         parentIdx1, parentIdx2 = gen_ops.randomSelection(population)
         chrom1, chrom2 = population[parentIdx1], population[parentIdx2]
-        # 交叉生成新子代种群
         childPop = gen_ops.crossover(chrom1, chrom2, config.CROSS_TIMES)
-        # 子代种群变异
         childPop = gen_ops.mutate(childPop, config.P_MUT)
-        # 将子代种群应用到 best_solution, 获取所有新解
         new_solutions, new_times = gen_ops.applyPopulation(childPop, best_solution, parameters)
-        # 计算子代适应度
         fitness = gen_ops.fitness(new_times)
-        # 接受更优接
         for i in range(len(new_solutions)):
             new_time = new_times[i]
             if new_time < best_time:
                 best_time = new_time
                 best_solution = new_solutions[i]
-        #子代选择两个个体
         childChrom1 = gen_ops.bestSelection(childPop, fitness)
         childChrom2 = gen_ops.rouletteSelection(childPop, fitness)
-        #替换所选的父代个体
         population[parentIdx1], population[parentIdx2] = childChrom1, childChrom2
     return best_time
 
