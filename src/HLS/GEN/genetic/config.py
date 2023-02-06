1d9c13a34dcf905313f9d940b8fd4b07430083a7
wurifu desktop <rifuwu123@gmail.com>
2023-02-07 01:10:20 +0800

    230207 禁忌搜索开关

--- src/HLS/GEN/genetic/config.py
+++ src/HLS/GEN/genetic/config.py
@@ -5,7 +5,7 @@ CHROM_LENGTH = 70
 GEN_NUM = 100
 
 PROBLEM_SET = ['MK01', 'MK02', 'MK03', 'MK04', 'MK05', 'MK06', 'MK07', 'MK08', 'MK09', 'MK10']
-PROBLEM = 'MK04'
+PROBLEM = 'MK06'
 PROBLEM_PATH = os.path.join(os.getcwd(), "../../../Brandimarte_Data/" + PROBLEM + ".fjs")
 
 P_MUT = 0.3
